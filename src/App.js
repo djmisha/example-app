200ff664b1cdbc6502448085ef4208e619cd2b17
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2019-06-05 07:35:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav>
+         Home
+        </nav>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.bizx.info/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Menu
         </a>
       </header>
+      <article className="App-main">
+          
+      </article>
+      <footer className="App-footer">
+        Thanks for visiting 
+      </footer>
     </div>
   );
 }
